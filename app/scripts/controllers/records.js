dda9cbb7efbe6275c04e0c4c3516ddc6fb1638ca
Leon Li <la2li@uwaterloo.ca>
2014-07-16 03:00:26 -0400

    Implemented remove record modal and functionality

--- app/scripts/controllers/records.js
+++ app/scripts/controllers/records.js
@@ -9,6 +9,7 @@
  */
 angular.module('moneyManagerApp')
   .controller('RecordsCtrl', function ($scope) {
+    // Initialize scope variables
     if (localStorage.getItem('moneyManagerRecords')) {
       console.log(localStorage.getItem('moneyManagerRecords'));
       $scope.records = JSON.parse(localStorage.getItem('moneyManagerRecords'));
@@ -25,10 +26,16 @@ angular.module('moneyManagerApp')
       saveRecord: function () {
         $scope.records.push($scope.draftRecord);
         $scope.draftRecord = {};
+      },
+      currentRecordIndex: function (index) {
+        $scope.currentRecordIndex = index;
+      },
+      deleteRecord: function (index) {
+        $scope.records.splice(index, 1);
       }
     };
 
-    $scope.$watch('records', function (newValue, oldValue) {
+    $scope.$watch('records', function (newValue) {
       console.log('# Saving moneyManagerRecords');
       localStorage.setItem('moneyManagerRecords', JSON.stringify(newValue));
     }, true);
