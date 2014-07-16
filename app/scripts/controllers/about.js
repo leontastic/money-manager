9ac071c23af2f4295b48d5ba35770e26f901dfe8
Leon Li <la2li@uwaterloo.ca>
2014-07-15 22:26:51 -0400

    Generated Angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moneyManagerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the moneyManagerApp
+ */
+angular.module('moneyManagerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
